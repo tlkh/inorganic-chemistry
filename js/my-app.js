e5b11c94649c76030bc8dc5b22bea7839b8b165a
Timothy Liu <tlkh@live.co.uk>
2016-09-29 20:33:50 +0800

    Initial Commit

---
+++ js/my-app.js
@@ -0,0 +1,8 @@
+var myApp = new Framework7();
+var $$ = Dom7;
+var leftView = myApp.addView('.view-left', {
+    dynamicNavbar: true
+});
+var mainView = myApp.addView('.view-main', {
+    dynamicNavbar: true
+});
